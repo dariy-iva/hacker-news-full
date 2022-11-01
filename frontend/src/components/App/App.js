6eb530bcde5043aa740fa8915ef5aebad3c5ac00
Дарья Иванова <dashann@mail.ru>
2022-11-01 13:31:42 +0300

    feat/init frontend app on react

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default connect(
+  (state) => ({}),
+  {}
+)(App);
